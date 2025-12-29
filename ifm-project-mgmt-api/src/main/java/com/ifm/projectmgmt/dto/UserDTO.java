134885f7357c3b67ba442b83aaba0048477ca566
kervinbalibagoso <31283189+khrlawliet@users.noreply.github.com>
2025-12-29 19:45:47 +0800

    change userDTO to record type

--- ifm-project-mgmt-api/src/main/java/com/ifm/projectmgmt/dto/UserDTO.java
+++ ifm-project-mgmt-api/src/main/java/com/ifm/projectmgmt/dto/UserDTO.java
@@ -1,15 +1,8 @@
 package com.ifm.projectmgmt.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserDTO {
-    private Long id;
-    private String username;
-    private String email;
-    private String fullName;
-}
+public record UserDTO(
+    Long id,
+    String username,
+    String email,
+    String fullName
+) {}
